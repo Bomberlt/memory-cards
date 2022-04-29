213be06694f941cd51d85562bd4d550f60937a35
Povilas Galcius <povilas.galcius@devbridge.com>
2022-04-29 14:22:05 +0300

    Card flip functionality

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Card.css'
 const Card = (props) => {
   const { id, value, suit, image} = props;
+  const [faceUp, flip] = useState(false);
+
+  if (!faceUp) {
+    return (
+      <div id={id} className="Card" onClick={() => flip(true)} >
+        face down
+        <img
+          src="https://deckofcardsapi.com/static/img/back.png"
+          alt="card back"
+        /> 
+      </div>
+    );
+  }
+
   return (
-    <div id={id} className="Card">
-      <span>{value} - {suit}</span>
+    <div id={id} className="Card"  onClick={() => flip(false)} >
+      <span>{value} {suit}</span>
       <div>
         <img src={image} alt={value+suit}/> 
       </div>
